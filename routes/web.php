f2c48b294f47b2091f66f50c72f07c8f53a43eee
Shashimii <besasamreginald36@gmail.com>
2025-05-18 15:46:05 +0800

    # dashboard route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AssignedDutyController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DutyController;
 use App\Http\Controllers\OfficerController;
 use App\Http\Controllers\ProfileController;
@@ -18,9 +19,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
