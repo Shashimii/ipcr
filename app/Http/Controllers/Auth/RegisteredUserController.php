057f596040da759f8b703681ff5afc480ef68f76
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-06-03 13:57:34 +0800

    * disable login on register

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -44,8 +44,8 @@ class RegisteredUserController extends Controller
 
         event(new Registered($user));
 
-        Auth::login($user);
+        // Auth::login($user); disable login on register
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('officers.index', absolute: false));
     }
 }
